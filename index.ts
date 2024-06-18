d5eb87aaa3da1885243f5f987defed0ad808964b
monsarkey <00smarkey@gmail.com>
2024-06-18 18:30:05 -0400

    cors configuration and tsconfig changes

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+
 import {Request, Response} from 'express'
 
 
@@ -7,6 +9,25 @@ const PORT: number = parseInt((process.env.PORT) ? process.env.PORT : '0');
 
 const app = express();
 const posts = require('./routes/posts');
+
+
+// TODO: FIGURE OUT CORS STUFF LATER, THIS IS PLACEHOLDER CODE THAT FUNCTIONS
+const allow = [process.env.BASE_URL]
+const corsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (allow.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+if (process.env.USE_CORS === 'y') {
+    app.use(cors(corsOptions))
+} else {
+    app.use(cors())
+}
 
 // setting up post routes defined in './routes/posts.ts'
 app.use(posts)
