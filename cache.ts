267f0b8288369fe60989e2d9fbc2729742df2444
monsarkey <00smarkey@gmail.com>
2024-07-22 14:47:50 -0400

    implemented cached search

--- cache.ts
+++ cache.ts
@@ -1,13 +1,15 @@
 
 import { TSGhostContentAPI } from '@ts-ghost/content-api';
-import { Post } from './types'
+import { Request, Response } from 'express'
+import { Post, PostMetadata, PostResponse } from './types';
 import { selectPostFields } from './util';
+import { match } from 'assert';
 
 
 export class PostCache {
 
-  private _postList: Post[];
-  private _cache: {};
+  private _postList: Post[] = [];
+  private _cache: Map<string, any>;
   private _api: TSGhostContentAPI;
 
   private _retry_num: number;
@@ -18,52 +20,100 @@ export class PostCache {
     this._api = api;
     this._retry_num = retry_num;
 
-    this._postList = this._getPosts() || [];
-
+    this._cache = new Map<string, any>();
+    this._getPosts();
   }
 
-  private _getPosts(): Post[] | null {
+  private _getPosts(): void  {
 
-    let attempt = 0;
+    this._api.posts
+      .browse()
+      .include({ tags: true })
+      .fetch()
+      .then((result) => {
+        if (result.success) {
+          this._setup_failure = false;
+          this._postList = result.data.map(selectPostFields);
+        } else {
+          console.log(`failed to fetch posts`)
+          this._setup_failure = true;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this._setup_failure = true;
+      })   
 
-    do {
-      this._api.posts
-        .browse()
-        .include({ tags: true })
-        .fetch()
-        .then((result) => {
-          if (result.success) {
-            return result.data.map(selectPostFields);
-          } else {
-            console.log(`failed to fetch posts, retrying. [${attempt}/${this._retry_num}] `)
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        })   
-      attempt += 1;
-    } while (attempt < this._retry_num)
-
-    console.log('failed to setup cache!');
-    this._setup_failure = true;
-    return;
-
-  }
-
-  private _search() {
-    
   }
 
   public refresh() {
-    this._cache = {};
-    this._postList = this._getPosts() || [];
+    // this._cache = {};
+    this._getPosts();
   }
 
-  public get(request) {
+  public search(count: number, page: number, search: string, tags: string[]): PostResponse | null {
+
+    // if posts aren't stored locally, we can't search via cache.
+    if (this._setup_failure)
+      return null;
+
+    let matchingPosts: Post[] = [];
+    let postMeta: PostMetadata;
+
+    this._postList.forEach((post) => {
+
+      let hasTags = false;
+      let hasQuery = false;
+
+      if(tags.length) {
+        const tagStringArr = post.tags?.map(tag => tag.slug) || [];
+
+        // if none of the queried tags exist in the post's tag array
+        if (tags.some((tag) => tagStringArr.includes(tag))) {
+          hasTags = true;
+        } 
+      }
+
+      if(search) {       
+        search = search.toLowerCase();
+
+        if ((post.title.toLowerCase().includes(search) || post.custom_excerpt.toLowerCase().includes(search))) {
+          hasQuery = true;
+        }
+      }
+
+      if (((tags.length && hasTags) && (search && hasQuery)) 
+        || (!tags.length && (search && hasQuery))
+        || ((tags.length && hasTags) && !search)
+        || (!tags.length && !search)) {
+          matchingPosts.push(post);
+        }
+
+    })
+
+    const totalPages = Math.ceil(matchingPosts.length / count);
+
+    postMeta = {
+      pages: totalPages,
+      page: page,
+      limit: count,
+      total: matchingPosts.length,
+      prev: (page - 1) || null,
+      next: (page < totalPages) ? page + 1 : null
+    }
+  
+    return {
+      posts: matchingPosts.slice((page-1)*count, page*count),
+      meta: postMeta
+    }
 
   }
 
-  public set(request) {
+  public get(request: Request) {
+
+  }
+
+  public set(request: Request): void {
 
   }
 }
